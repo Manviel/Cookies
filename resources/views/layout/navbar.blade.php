df633f4a08da03e7f5c723d4e967a19362b1b721
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-01-04 15:39:06 +0200

    Fetching and displaying Data. Add pagination for pages

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,6 +3,7 @@
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li><a href="/">Home</a></li>
+                <li><a href="/posts">Blog</a></li>
                 <li><a href="/about">About</a></li>
             </ul>
         </div>
